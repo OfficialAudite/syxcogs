d285a8ce25456bbdd0aa9e525fe73b7f809dafb7
leakdev <info@leakoni.net>
2018-06-12 22:37:42 +0200

    Pout added

---
+++ pout/pout.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import errors, converter
+from .utils.dataIO import fileIO
+from random import choice as rnd
+
+class Pout:
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.pout_images = fileIO("data/syxactions/pout.json","load")
+
+    @commands.command(pass_context=True)
+    async def pout(self, ctx, user: discord.Member=None):
+        """It's a action if you want to pout. Tag a person too!"""
+
+        author = ctx.message.author
+        try:
+
+                b = discord.Embed(color = discord.Color(0xA4DAC4)))
+                b.set_image(url=rnd(self.pout_images))
+                await self.bot.say(embed=b)
+
+        except errors.BadArgument:
+                await self.bot.say("Oops, a pout picture couldn't be sent! Try again later")
+
+def setup(bot):
+    bot.add_cog(Pout(bot))
