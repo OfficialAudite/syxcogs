ae87e37f6d58a3f95b7fc518b0228fef618133f7
leakdev <info@leakoni.net>
2018-06-14 18:05:31 +0200

    Pat & Kiss added

---
+++ kiss/kiss.py
@@ -0,0 +1,34 @@
+from discord.ext import commands
+from discord.ext.commands import errors, converter
+from .utils.dataIO import fileIO
+import random
+import discord
+
+class Kiss:
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(pass_context=True)
+    async def kiss(self, context, member: discord.Member=None):
+        """Kiss your senpai/waifu or even the enemy!"""
+        author = context.message.author.mention
+        text = ("a ghost!!")
+        
+        if member != None:
+            mention = member.mention
+            text = mention
+
+        kiss = "**{0} kisses {1}!**"
+
+        choices = fileIO("data/kiss/syxactions/kiss.json","load")		
+                
+        image = random.choice(choices)
+        
+        embed = discord.Embed(description=kiss.format(author, text), colour=discord.Colour.blue())
+        embed.set_image(url=image)
+
+        await self.bot.say(embed=embed)
+
+def setup(bot):
+    n = Kiss(bot)
+    bot.add_cog(n)
---
+++ pat/pat.py
@@ -0,0 +1,34 @@
+from discord.ext import commands
+from discord.ext.commands import errors, converter
+from .utils.dataIO import fileIO
+import random
+import discord
+
+class Pat:
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(pass_context=True)
+    async def pat(self, context, member: discord.Member=None):
+        """Pat your senpai/waifu or even the enemy!"""
+        author = context.message.author.mention
+        text = ("a ghost!!")
+        
+        if member != None:
+            mention = member.mention
+            text = mention
+
+        pat = "**{0} kisses {1}!**"
+
+        choices = fileIO("data/pat/syxactions/pat.json","load")		
+                
+        image = random.choice(choices)
+        
+        embed = discord.Embed(description=pat.format(author, text), colour=discord.Colour.blue())
+        embed.set_image(url=image)
+
+        await self.bot.say(embed=embed)
+
+def setup(bot):
+    n = Pat(bot)
+    bot.add_cog(n)
