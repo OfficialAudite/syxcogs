ce0d4f7feebf9156ce4773da2b25ac2e83a73889
leakdev <info@leakoni.net>
2018-06-14 16:44:31 +0200

    Minor Update
    Fixed so in the embed image there is person tags too.

--- hug/hug.py
+++ hug/hug.py
@@ -1,37 +1,30 @@
-import discord
 from discord.ext import commands
 from discord.ext.commands import errors, converter
 from .utils.dataIO import fileIO
-from random import choice as rnd
+import random
+import discord
 
 class Hug:
-
     def __init__(self, bot):
         self.bot = bot
-        self.hug_images = fileIO("data/hug/syxactions/hug.json","load")
 
     @commands.command(pass_context=True)
-    async def hug(self, ctx, user: discord.Member=None):
-        """It's a action if you want to hug. Tag a person too!"""
-
-        #author = ctx.message.author
+    async def hug(self, context, member: discord.Member):
+        """Hug your senpai/waifu!"""
+        author = context.message.author.mention
+        mention = member.mention
         
-        author = str(ctx.message.author)
-        author_name, author_code = author.split("#")
-		
-        target = "himself, creepy ugghhh!!!!!"
-        if user != None:
-            user = str(user)
-            user_name, user_code = user.split("#")
-            target = user_name
+        hug = "**{0} hugs {1} !**"
 
-        try:
-                b = discord.Embed(color = discord.Color(0xA4DAC4), title = (author_name + " hugs " + target))
-                b.set_image(url=rnd(self.hug_images))
-                await self.bot.say(embed=b)
+        choices = fileIO("data/hug/syxactions/hug.json","load")		
+                
+        image = random.choice(choices)
+        
+        embed = discord.Embed(description=hug.format(author, mention), colour=discord.Colour.blue())
+        embed.set_image(url=image)
 
-        except errors.BadArgument:
-                await self.bot.say("Oops, a hug picture couldn't be sent! Try again later")
+        await self.bot.say(embed=embed)
 
 def setup(bot):
-    bot.add_cog(Hug(bot))
+    n = Hug(bot)
+    bot.add_cog(n)
