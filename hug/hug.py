2cf337f984adcfcd3fe473351a1948cc094153ab
leakdev <info@leakoni.net>
2018-06-14 17:07:28 +0200

    Same as bite

--- hug/hug.py
+++ hug/hug.py
@@ -9,18 +9,22 @@ class Hug:
         self.bot = bot
 
     @commands.command(pass_context=True)
-    async def hug(self, context, member: discord.Member):
+    async def hug(self, context, member: discord.Member=None):
         """Hug your senpai/waifu!"""
         author = context.message.author.mention
-        mention = member.mention
+        text = ("himself, creepy ugghhh!!!!!")
         
-        hug = "**{0} hugs {1} !**"
+        if member != None:
+            mention = member.mention
+            text = mention
+
+        hug = "**{0} hugs {1}!**"
 
         choices = fileIO("data/hug/syxactions/hug.json","load")		
                 
         image = random.choice(choices)
         
-        embed = discord.Embed(description=hug.format(author, mention), colour=discord.Colour.blue())
+        embed = discord.Embed(description=hug.format(author, text), colour=discord.Colour.blue())
         embed.set_image(url=image)
 
         await self.bot.say(embed=embed)
