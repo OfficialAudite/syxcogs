4ccada1bc1ed435661d7c602b1593bfae1e7c617
leakdev <info@leakoni.net>
2018-06-17 00:16:45 +0200

    update

--- ping/ping.py
+++ ping/ping.py
@@ -1,8 +1,9 @@
 import discord
 from discord.ext import commands
+from .utils.dataIO import fileIO
 import datetime
 import time
-from random import choice, randint
+from random import choice, randint, random
 
 class ping:
     """Ping, with time"""
@@ -13,6 +14,8 @@ class ping:
     @commands.command(pass_context=True)
     async def ping(self, ctx):
         """Pong."""
+        choices = fileIO("data/ping/syxactions/ping.json","load")
+        image = random.choice(choices)
         t1 = time.perf_counter()
         await self.bot.send_typing(ctx.message.channel)
         t2 = time.perf_counter()
@@ -20,7 +23,7 @@ class ping:
         color = ''.join([choice('0123456789ABCDEF') for x in range(6)])
         color = int(color, 16)
         data = discord.Embed(title=("Pong"), description=thedata, colour=discord.Colour(value=color))
-        data.set_image(url="http://touch.prodigy.co.id/wp-content/uploads/2015/04/36.gif")
+        data.set_image(url="image")
         
         await self.bot.say(embed=data)
      
